aba7a595705cab501ffb090fd50147a5d26ece50
Daryl Marco <darmarco@gmail.com>
2020-07-01 21:52:04 -0700

    Added submit page."

---
+++ src/components/Submit.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Submit extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return(
+      <div>
+        This is submit
+      </div>
+    )
+  }
+}
+
+export default Submit;
