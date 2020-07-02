2f8daa08e0a8d5ed1f4c20e34780b556bd6ac49a
Daryl Marco <darmarco@gmail.com>
2020-07-01 21:50:08 -0700

    Added handling for react-router

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3003;
 
-app.use('/', express.static(__dirname + '/../dist'))
+
+app.use('/', express.static(__dirname + '/../dist/'))
+app.use('/:id', express.static(__dirname + '/../dist/'))
 
 app.listen(port, ()=>console.log(`Started ReaperContest server on port ${port} at ${new Date()}`))
