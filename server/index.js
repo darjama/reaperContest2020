e4a1f22345195d5bf2de636b034529d79f2517eb
Daryl Marco <darmarco@gmail.com>
2020-07-03 23:14:01 -0700

    Set up connection to ftp server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,30 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express');
 const app = express();
-const port = 3003;
+const fs = require('fs')
+const cors = require('cors');
+const multer = require('multer');
+const port = process.env.PORT || 3003;
+let Client = require('ssh2-sftp-client');
+let sftp = new Client();
 
+sftp.connect({
+  host: process.env.FTPHOST,
+  port:  process.env.FTPPORT,
+  username: process.env.FTPUN,
+  password: process.env.FTPPW
+}).then(() => {
+  return sftp.list('/');
+}).then(data => {
+  console.log(data, 'the data info');
+}).catch(err => {
+  console.log(err, 'catch error');
+});
+
+
+app.use(cors());
 
 app.use('/', express.static(__dirname + '/../dist/'))
 app.use('/:id', express.static(__dirname + '/../dist/'))
