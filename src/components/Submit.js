1369f3cdace7e955acb499c829807e14c7a07544
Daryl Marco <darmarco@gmail.com>
2020-07-07 22:40:35 -0700

    Fixed change and click handlers

--- src/components/Submit.js
+++ src/components/Submit.js
@@ -1,13 +1,53 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import bsCustomFileInput from 'bs-custom-file-input';
+import axios from 'axios';
 
 class Submit extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFile: null
+    }
   }
+
+  componentDidMount() {
+    bsCustomFileInput.init()
+  }
+
+
+
+  onChangeHandler(event) {
+    this.setState({selectedFile: event.target.files[0]})
+
+  }
+
+  clickHandler(e) {
+    e.preventDefault();
+    const data = new FormData()
+    data.append('file', this.state.selectedFile)
+    axios.post("/upload", data, { // receive two parameter endpoint url ,form data
+      })
+      .then(res => { // then print response status
+        console.log(res.statusText)
+      })
+  }
+
   render() {
     return(
-      <div>
-        This is submit
+      <div style={{width: '50%'}}>
+        Upload Your File
+        <Form>
+          <Form.File
+            id="custom-file"
+            label="Custom file input"
+            custom
+            onChange={() => this.onChangeHandler(event)}
+          />
+          <Button variant="primary" type="submit" onClick={() => this.clickHandler(event)}>
+              Submit
+          </Button>
+        </Form>
       </div>
     )
   }
