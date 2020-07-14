1a923e02369d522ac84c52847e88090c14e6dcb8
Daryl Marco <darmarco@gmail.com>
2020-07-13 20:55:27 -0700

    Added contest info controller

---
+++ db/contestController.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose'),
+Contest = mongoose.model('Contests');
+
+exports.contests = function(req, res) {
+  Contest.find({}, function(err, contest) {
+    if (err)
+      res.send(err);
+    res.json(contest);
+  });
+};
+
+exports.contest = function(req, res) {
+  const month = req.params.month;
+  const year = req.params.year;
+  Contest.findOne({ month, year }, function(err, contest) {
+    if (err)
+      res.send(err);
+    res.json(contest);
+  });
+};
