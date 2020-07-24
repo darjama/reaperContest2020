571ee0a65805c16c522be0fb7b6f4547cc9a4d62
Daryl Marco <darmarco@gmail.com>
2020-07-23 23:52:13 -0700

    Separated Loader into own component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from "redux-thunk";
@@ -9,12 +9,14 @@ import App from './components/Home/App.js';
 import NavBar from './components/Navbar.js';
 import Vote from './components/Vote/Vote.js';
 import Submit from './components/Submit/Submit.js';
+import Loader from './components/Loader';
 
 const app = document.getElementById('app');
 const store = createStore(combinedReducers, applyMiddleware(thunk));
 
 ReactDOM.render(
 <Provider store={store}>
+  <Loader/>
   <BrowserRouter>
     <NavBar/>
       <Route exact path="/" component={App} />
