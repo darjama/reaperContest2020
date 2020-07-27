79c335d66aa4fa948df7ee3397c65cecf79bd005
Daryl Marco <darmarco@gmail.com>
2020-07-27 16:07:10 -0700

    Added Hero jumbotron component.

---
+++ src/components/common/Hero.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Jumbotron, Container } from 'react-bootstrap';
+import Background from '../../../dist/assets/reaper-panel.png';
+
+const Hero = function(props) {
+  return (
+    <Jumbotron fluid style={{
+      backgroundImage: ` radial-gradient(circle, transparent 40%, black 75%), url(${Background})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      color: 'yellow',
+      minHeight: '25vh'
+      }}>
+      <Container>
+        <h1 className='cuprum rem4'>{props.name}</h1>
+      </Container>
+    </Jumbotron>
+  )
+}
+
+export default Hero;
