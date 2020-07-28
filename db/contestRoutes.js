a1ad1cbe2562b01274e66b4b780e548404e95005
Daryl Marco <darmarco@gmail.com>
2020-07-28 14:52:08 -0700

    Added footer component with associated styles

--- db/contestRoutes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-  var contest = require('./contestController');
-  app.route('/api/contests')
-      .get(contest.contests)
-  app.route('/api/contests/:month/:year')
-      .get(contest.contest)
-};
--- db/dlLogRoutes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-  var dlLog = require('./dlLogController');
-  app.route('/api/dlLog')
-      .get(dlLog.readLog)
-  app.route('/api/dlLog')
-      .post(dlLog.addLog)
-};
