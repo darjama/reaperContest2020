5a3225921415bd29eba08cbf3766e321712d58e4
Daryl Marco <darmarco@gmail.com>
2020-08-20 14:43:58 -0700

    Added actions for managing playlist and rerendering.

--- src/redux/playlist/playlistTypes.js
+++ src/redux/playlist/playlistTypes.js
@@ -1,1 +1,8 @@
-export const PLAY_NOW = 'PLAY_NOW'
+export const PLAY_NOW = 'PLAY_NOW';
+export const SHARE_NODE = 'SHARE_NODE';
+export const SHARE_NODE2 = 'SHARE_NODE2';
+export const SET_RERENDER = 'SET_RERENDER';
+export const ADD_NODE = 'ADD_NODE';
+export const MOVE_NODE = 'MOVE_NODE';
+export const DELETE_NODE = 'DELETE_NODE';
+export const CREATE_PLAYLIST = 'CREATE_PLAYLIST';
