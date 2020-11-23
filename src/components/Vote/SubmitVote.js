7b1343bbc26acac79c5b01f8f6ebbcfae5f05d1f
Daryl Marco <darmarco@gmail.com>
2020-11-22 20:15:02 -0800

    Condensed SubmitVote layout.

--- src/components/Vote/SubmitVote.js
+++ src/components/Vote/SubmitVote.js
@@ -3,15 +3,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import { clearVotes } from '../../redux/voting/voteActions';
 import { clearNotes } from '../../redux/notes/notesActions'
 import { playLink } from '../../redux/playlist/playlistActions';
+import validateEmail from '../common/SharedFormulas';
 import { ListGroup, Form, Button } from 'react-bootstrap';
 import axios from 'axios';
 
-var SubmitVote = function() {
+var SubmitVote = function({voter}) {
   const notesAll = useSelector(state => state.noteReducer)
   const top3 = useSelector(state => state.voteReducer)
   const dispatch = useDispatch();
 
   const [includeNotes, setIncludeNotes] = useState(false);
+  const [email, setEmail] = useState('');
   const [disableSubmit, setDisableSubmit] = useState(false);
   const contestId = ((new Date()).getFullYear()* 100 + (new Date()).getMonth() + 1).toString();
 
@@ -21,7 +23,8 @@ var SubmitVote = function() {
     setDisableSubmit(true);
     let [first, second, third] = top3;
     const notes = includeNotes ? notesAll : {} ;
-    axios.post('/api/addVote', {first, second, third, notes})
+    const respondant = voter ? voter.mixnum + '-' + voter.contestant : email;
+    axios.post('/api/addVote', {first, second, third, notes, respondant})
       .then(res => {
         localStorage.setItem('lastVoted', contestId)
       })
@@ -37,28 +40,48 @@ var SubmitVote = function() {
   if (!disableSubmit) {
     return (
       <div className='submitvote'>
-        <ListGroup>
-        <ListGroup.Item variant='dark'><h1>Submit Your Vote:</h1></ListGroup.Item>
-          <ListGroup.Item variant='dark'>
+        <ListGroup >
+    <ListGroup.Item variant='dark'><h4>
+      {!!voter && voter.contestant + ' - ' }Submit Your Vote:
+    </h4></ListGroup.Item>
+          <ListGroup.Item variant='dark' className="submit-list">
             1st place (3 points):
             <span style={{color: 'red'}}>{top3[0] ? ` Mix# ${top3[0]}` : ''}</span>
           </ListGroup.Item>
-          <ListGroup.Item variant='dark'>
+          <ListGroup.Item variant='dark' className="submit-list">
             2nd place (2 points):
             <span style={{color: 'red'}}>{top3[1] ? ` Mix# ${top3[1]}` : ''}</span>
           </ListGroup.Item>
-          <ListGroup.Item variant='dark'>
+          <ListGroup.Item variant='dark' className="submit-list">
             3rd place (1 point):
             <span style={{color: 'red'}}>{top3[2] ? ` Mix# ${top3[2]}` : ''}</span>
           </ListGroup.Item>
-          <ListGroup.Item variant='dark' className='playerparent'>
-            <Form>
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Include your notes with your vote" onClick={()=>setIncludeNotes(!includeNotes)}/>
+          <ListGroup.Item variant='dark' className='submit-list playerparent'>
+
+            {voter === undefined  && (
+              <>
+              <Form>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Control type="email" placeholder="Enter email (required)" value={email} onChange={(e)=>setEmail(e.target.value)}/>
               </Form.Group>
+              </Form>
+              &nbsp;&nbsp;
+              </>
+            )}
+            <Form inline>
+              <Form.Group controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" onClick={()=>setIncludeNotes(!includeNotes)}/>
+                <Form.Label>Include your notes with your vote</Form.Label>
+              </Form.Group>
+              &nbsp;&nbsp;
             </Form>
             <div style={{margin:'0 0 0 auto'}}>
-              <Button variant="primary" type="submit" disabled={top3.includes(null)} onClick={() => submitHandler(event)}> Submit </Button>
+              <Button
+                variant="primary" type="submit"
+                disabled={top3.includes(null) || (voter === undefined ? !validateEmail(email) : false)}
+                onClick={() => submitHandler(event)}>
+                  Submit
+              </Button>
             </div>
 
           </ListGroup.Item>
