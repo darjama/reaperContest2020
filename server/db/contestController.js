bb2b14efc8768d8d7e1620a60d0ac8da21b65f5e
Daryl Marco <darmarco@gmail.com>
2020-12-19 16:23:31 -0800

    Added resultsContest api

--- server/db/contestController.js
+++ server/db/contestController.js
@@ -13,8 +13,21 @@ exports.contest = function(req, res) {
   const month = req.params.month;
   const year = req.params.year;
   Contest.findOne({ month, year }, function(err, contest) {
-    if (err)
-      res.send(err);
+    if (err) return res.send(err);
+    res.json(contest);
+  });
+};
+
+exports.resultsContest = function(req, res) {
+  Contest.findOne({ resultdate: {$lt: new Date()} }, null, {sort: {resultdate: -1}}, function(err, contest) {
+    if (err) return res.send(err);
+    res.json(contest);
+  });
+};
+
+exports.contestNames = function(req, res) {
+  Contest.find( {}, ['contestid', 'contestlabel'], {sort: {'contestid': -1}}, function(err, contest) {
+    if (err) return res.send(err);
     res.json(contest);
   });
 };
