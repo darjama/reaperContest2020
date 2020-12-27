f90c7aa319a137726b5a5be23243bd83ee0dfdaf
Daryl Marco <darmarco@gmail.com>
2020-12-27 15:34:02 -0800

    Revised entries controller and model to accept double precision for offset and normalize values.

--- server/db/entriesController.js
+++ server/db/entriesController.js
@@ -1,47 +1,58 @@
 const mongoose = require('mongoose'),
-Entry = mongoose.model('Entries');
+  Entry = mongoose.model('Entries');
 
-exports.addEntry  = function(req, res) {
-  console.log(req.body)
+exports.addEntry = function (req, res) {
   const newRecord = new Entry();
-  newRecord.ipaddr = req.headers['x-forwarded-for'] ||
-  req.connection.remoteAddress ||
-  req.socket.remoteAddress ||
-  (req.connection.socket ? req.connection.socket.remoteAddress : null);
+  newRecord.ipaddr =
+    req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    (req.connection.socket ? req.connection.socket.remoteAddress : null);
   newRecord.timestamp = new Date();
   newRecord.email = req.body.email;
   newRecord.message = req.body.message;
   newRecord.zipfile = req.body.filename;
-  newRecord.contestid = (newRecord.timestamp.getYear() + 1900) * 100 + (newRecord.timestamp.getMonth() + 1);
+  newRecord.contestid =
+    (newRecord.timestamp.getYear() + 1900) * 100 +
+    (newRecord.timestamp.getMonth() + 1);
   newRecord.uploadSuccessful = req.body.success;
-  newRecord.mixnum = null;
-  newRecord.contestant = null;
-  newRecord.audiouri = null;
-  newRecord.save({},function(err, data) {
-    if (err){
+  newRecord.mixnum = 0;
+  newRecord.contestant = '';
+  newRecord.offset = 0;
+  newRecord.normalize = 0;
+  newRecord.save({}, function (err, data) {
+    if (err) {
       console.log(err); //changed from res.send to accomodate separate upload
       return res.status(500).send(err);
     }
     console.log(data); //changed from res.send to accomodate separate upload
     res.json(data);
-  })
+  });
 };
 
+exports.entries = function (req, res) {
+  Entry.find(
+    { contestid: req.params.contestid, mixnum: { $ne: null } },
+    {},
+    { sort: { mixnum: 1 } },
+    function (err, list) {
+      if (err) res.send(err);
+      res.json(list);
+    }
+  );
+};
 
-exports.entries = function(req, res) {
-  Entry.find({contestid: req.params.contestid,  mixnum: {$ne: null}}, {}, {sort: {mixnum: 1}}, function(err, list) {
-    if (err)
-      res.send(err);
-    res.json(list);
-  });
-}
-
-exports.entriesAnon = function(req, res) {
+exports.entriesAnon = function (req, res) {
   const month = '0' + req.params.month;
-  const contestid = '' + req.params.year +  month.substring(month.length -2);
-  Entry.find({contestid, mixnum: {$ne: null}}, 'mixnum audiouri contestid', {sort: {mixnum: 1}}, function(err, list) {
-    if (err) res.send(err);
-    list.sort((a,b) => a-b)
-    return res.json(list);
-  });
-}
+  const contestid = '' + req.params.year + month.substring(month.length - 2);
+  Entry.find(
+    { contestid, mixnum: { $ne: null } },
+    'mixnum audiouri contestid',
+    { sort: { mixnum: 1 } },
+    function (err, list) {
+      if (err) res.send(err);
+      list.sort((a, b) => a - b);
+      return res.json(list);
+    }
+  );
+};
--- server/upload/receiveController.js
+++ server/upload/receiveController.js
@@ -1,27 +1,35 @@
-const {Storage} = require('@google-cloud/storage');
+const { Storage } = require('@google-cloud/storage');
 
-var storage = new Storage()
+var storage = new Storage();
 
-const getSignedUploadUrl = async function(filename, bucket) {
+const getSignedUploadUrl = async function (filename, bucket) {
   const options = {
     version: 'v4',
     action: 'write',
     expires: Date.now() + 10 * 60 * 1000, // 10 minutes
     contentType: 'application/octet-stream',
-  }
+  };
   const date = new Date();
-  const contestId = date.getFullYear().toString() + (date.getMonth() > 8 ? '': '0') +(date.getMonth() + 1).toString();
+  const contestId =
+    date.getFullYear().toString() +
+    (date.getMonth() > 8 ? '' : '0') +
+    (date.getMonth() + 1).toString();
   try {
-  const [url] = await storage.bucket(bucket).file(`upload/${contestId}/${filename}`).getSignedUrl(options);
-  return url;
-  } catch (err) {console.log(err)}
-}
-
-exports.processUpload = function(req, res, next) {
-  getSignedUploadUrl(req.query.filename, "mindful-pillar-284616.appspot.com")
-  .then(url => res.status(200).send(url))
-  .catch(err => {
+    const [url] = await storage
+      .bucket(bucket)
+      .file(`upload/${contestId}/${filename}`)
+      .getSignedUrl(options);
+    return url;
+  } catch (err) {
     console.log(err);
-    return res.status(500).send(err);
-  });
-}
+  }
+};
+
+exports.processUpload = function (req, res, next) {
+  getSignedUploadUrl(req.query.filename, 'mindful-pillar-284616.appspot.com')
+    .then((url) => res.status(200).send(url))
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).send(err);
+    });
+};
